991a42f58678cbf28039f60e772cb531da82cfab
jamalull <jamaluldazzled@gmail.com>
2023-05-24 17:41:05 +0700

    add task on week 3 - create calcualtor apps

---
+++ week-3/calculator-apps/assets/js/index.js
@@ -0,0 +1,30 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+//Define function to calculate based on button clicked
+const calculate = (btnValue) => {
+    if(btnValue === "=" && output !== "") {
+        // If output has '%', then replace with '/100' before evaluating.
+        // This is use eval() function which is the function can execute any command from string.
+        // output = eval(output.replace("%", "/100")); //this function for create percentage calculate, just change '%' with '/100'
+        output = eval(output); //this function for create modulus calculate
+    } else if(btnValue === "AC") {
+        output = "";
+    } else if(btnValue === "DEL") {
+        //If DEL btn is clicked, then remove the last character from the output.
+        output = output.toString().slice(0, -1);
+    } else {
+        // If output is empty and button is specialChars then return.
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
+
+// Add event listener to buttons, and then call calculate() on click
+buttons.forEach((button) => {
+    // Button clickc listener calls, calculate() with dasaet value as argument 
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
